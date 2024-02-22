f6c0918c0b41cf190c80dc75196a3578d3d30c46
TheGuyCheese <s.aditya.digi@gmail.com>
2024-02-22 11:40:08 +0530

    accepts all characters

--- Hashing/characterHashing.cpp
+++ Hashing/characterHashing.cpp
@@ -6,10 +6,13 @@ int main(){
     cin>>s;
     int query;
 
-    int hash[26] = {0};
+    int hash[256] = {0};
+
+    //int hash[26] = {0} for only the small alphabets or big alphabets
 
     for(int i =0; i<s.size(); i++){
-        hash[s[i]-'a']++;
+        hash[s[i]]++;
+        //hash[s[i]-'a'] for small alphabets and hash[s[i]-'A'] for big alphabets only
     }
 
     cin>>query;
@@ -17,7 +20,7 @@ int main(){
         char c;
         cin>>c;
         //fetch
-        cout<<hash[c-'a']<<endl;
+        cout<<hash[c]<<endl;
     }
     return 0;
 }
