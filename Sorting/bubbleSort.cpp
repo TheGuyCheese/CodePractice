39c565588e53fcdf71cbb23623b6dadbf0647ac3
TheGuyCheese <s.aditya.digi@gmail.com>
2024-03-05 22:36:27 +0530

    added some optimizations

--- Sorting/bubbleSort.cpp
+++ Sorting/bubbleSort.cpp
@@ -5,6 +5,8 @@ void bubbleSort(int arr[], int n)
 {
     for (int i = n - 1; i >= 1; i--)
     {
+        // adding optimizations so that the loop breaks if the array is in already sorted order
+        int swapCheck = 0;
         for (int j = 0; j <= i - 1; j++)
         {
             if (arr[j] > arr[j + 1])
@@ -12,8 +14,12 @@ void bubbleSort(int arr[], int n)
                 int temp = arr[j + 1];
                 arr[j + 1] = arr[j];
                 arr[j] = temp;
+                swapCheck = 1;
             }
         }
+        // function to break the loop if it is already sorted after the first pass
+        if (swapCheck == 0)
+            break;
     }
 }
 
